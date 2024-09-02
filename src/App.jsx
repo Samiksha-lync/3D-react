77b65b15713ef5925dc46f4e3658105ee1bf6dec
Samiksha-lync <Samiksha@lync.world>
2024-09-02 11:32:48 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Spline from './Spline';
 function App() {
   return (
     <div className="App">
-      <h1>My 3D Raeact project</h1>
+      <h1>My 3D React project</h1>
       <Spline />
     </div>
   );
